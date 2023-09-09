c613aeb5f3fb087d8f52c11812e8e22e03db6685
AntonyUsoltsev <usoltsev_anton1@mail.ru>
2023-09-09 10:26:14 +0700

    init commit

---
+++ OS/lab1_threads/threads/thread.c
@@ -0,0 +1,30 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void *mythread(void *arg) {
+	printf("mythread [%d %d %d]: Hello from mythread!\n", getpid(), getppid(), gettid());
+	return NULL;
+}
+
+int main() {
+	pthread_t tid;
+	int err;
+
+	printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
+	for (int i = 0; i < 5; i++) {
+		err = pthread_create(&tid, NULL, mythread, NULL);
+		if (err) {
+			printf("main: pthread_create() failed: %s\n", strerror(err));
+			return -1;
+		}
+	}
+	sleep(10);
+
+	return 0;
+}
+
