b655d67fdfd469b956dd6f6f9a08a20636dfd45e
AntonyUsoltsev <usoltsev_anton1@mail.ru>
2023-12-01 15:15:31 +0700

    bug fix in os lab3

--- OS/lab3_proxy/main.c
+++ OS/lab3_proxy/main.c
@@ -13,7 +13,10 @@
 #define GREEN "\033[1;32m"
 #define BLUE  "\033[1;34m"
 #define RESET "\033[0m"
+#define BACK_WHITE "\033[1;47m"
+#define BACK_PURP "\033[1;45m"
 
+#define FAIL (-1)
 #define PORT 80
 #define MAX_USERS_COUNT 10
 #define MAX_CACHE_SIZE 128
@@ -69,17 +72,13 @@ void sigint_handler(int signo) {
 int init_cache() {
     for (int i = 0; i < MAX_CACHE_SIZE; ++i) {
         cache[i].request = calloc(BUFFER_SIZE, sizeof(char));
-        if (cache[i].request == NULL) {
-            logg("Error in malloc while init cache", RED);
-            return -1;
-        }
         cache[i].response = calloc(BUFFER_SIZE * 10, sizeof(char));
-        if (cache[i].response == NULL) {
-            logg("Error in malloc while init cache", RED);
-            return -1;
+        if (cache[i].request == NULL || cache[i].response == NULL) {
+            logg("Error in сalloc while init cache", RED);
+            return FAIL;
         }
     }
-    return 1;
+    return EXIT_SUCCESS;
 }
 
 void destroy_cache() {
@@ -89,23 +88,54 @@ void destroy_cache() {
     }
 }
 
-char *read_request(int client_socket) {
+int create_server_socket() {
+    struct sockaddr_in server_addr;
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        logg("Error while creating server socket", RED);
+        return FAIL;
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_UNSPEC;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+    logg("Server socket created", GREEN);
+
+    int err = bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    if (err == FAIL) {
+        logg("Failed to bind server socket", RED);
+        close(server_socket);
+        return FAIL;
+    }
+
+    logg_int("Server socket bound to", server_addr.sin_addr.s_addr, GREEN);
+
+    err = listen(server_socket, MAX_USERS_COUNT);
+    if (err == FAIL) {
+        logg("Server socket failed to listen", RED);
+        close(server_socket);
+        return FAIL;
+    }
+    return server_socket;
+}
+
+int read_request(int client_socket, char *request) {
     // Get HTTP-request from client
-    char *request = calloc(BUFFER_SIZE, sizeof(char));
     ssize_t bytes_read = read(client_socket, request, BUFFER_SIZE);
     if (bytes_read < 0) {
         logg("Error while read request", RED);
         close(client_socket);
-        return NULL;
+        return EXIT_FAILURE;
     }
     if (bytes_read == 0) {
         logg("Connection closed from client", RED);
         close(client_socket);
-        return NULL;
+        return EXIT_FAILURE;
     }
     request[bytes_read] = '\0';
     logg_char("Received request:\n", request, GREEN);
-    return request;
+    return EXIT_SUCCESS;
 }
 
 int find_in_cache(char *request, int client_socket) {
@@ -124,22 +154,22 @@ int find_in_cache(char *request, int client_socket) {
     if (cache_index != -1) {
         // If response find in cache send it to client
         ssize_t send_bytes = send(client_socket, cache_record, strlen(cache_record), 0);
-        if (send_bytes == -1) {
+        if (send_bytes == FAIL) {
             logg("Error while sending cached data", RED);
             close(client_socket);
             free(cache_record);
-            return 0;
+            return EXIT_FAILURE;
         }
         free(cache_record);
-        logg_int("Sent cached response to the client, len = ", send_bytes, BLUE);
+        logg_int("Send cached response to the client, len = ", send_bytes, BLUE);
         printf("\n");
         close(client_socket);
-        return 1;
+        return EXIT_SUCCESS;
     }
-    return 0;
+    return EXIT_FAILURE;
 }
 
-int connect_to_remote(char * host){
+int connect_to_remote(char *host) {
     struct addrinfo hints, *res0;
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
@@ -153,17 +183,17 @@ int connect_to_remote(char * host){
         return -1;
     }
     int dest_socket = socket(res0->ai_family, res0->ai_socktype, res0->ai_protocol);
-    if (dest_socket == -1) {
+    if (dest_socket == FAIL) {
         logg("Error while creating remote server socket", RED);
-        return -1;
+        return FAIL;
     }
 
     int err = connect(dest_socket, res0->ai_addr, res0->ai_addrlen);
-    if (err == -1) {
+    if (err == FAIL) {
         logg("Error while connecting to remote server", RED);
         close(dest_socket);
         freeaddrinfo(res0);
-        return -1;
+        return FAIL;
     }
     return dest_socket;
 }
@@ -177,20 +207,21 @@ void *client_handler(void *arg) {
     strcpy(request, request0);
 
     // Creating new connection with remote server
-    // Parse the request and extract the host and port
-    logg("Create new connection with remote server", GREEN);
+    // Parse the request and extract the host
+
     unsigned char host[50];
     const unsigned char *host_result = memccpy(host, strstr((char *) request, "Host:") + 6, '\r', sizeof(host));
     host[host_result - host - 1] = '\0';
-    logg_char("Remote server host name: ", (char *)host, GREEN);
+    logg_char("Remote server host name: ", (char *) host, GREEN);
 
-    int dest_socket = connect_to_remote((char *)host);
-    if(dest_socket == -1){
+    int dest_socket = connect_to_remote((char *) host);
+    if (dest_socket == FAIL) {
         close(client_socket);
     }
+    logg("Create new connection with remote server", GREEN);
 
     ssize_t bytes_sent = write(dest_socket, request, sizeof(request));
-    if (bytes_sent == -1) {
+    if (bytes_sent == FAIL) {
         logg("Error while sending request to remote server", RED);
         close(client_socket);
         close(dest_socket);
@@ -198,6 +229,7 @@ void *client_handler(void *arg) {
     }
     logg_int("  Send request to remote server, len = ", bytes_sent, GREEN);
 
+    // Cache the request and shift current position in cache array
     pthread_mutex_lock(&cache_mutex);
     int cache_pos = cur_cache_pos;
     cur_cache_pos = (cur_cache_pos + 1) % MAX_CACHE_SIZE;
@@ -218,6 +250,8 @@ void *client_handler(void *arg) {
         } else {
             logg_int("\t  Write response to client, len = ", bytes_sent, GREEN);
             buffer[bytes_read] = '\0';
+
+            // Cache part of response
             pthread_mutex_lock(&cache_mutex);
             strcat(cache[cache_pos].response, buffer);
             pthread_mutex_unlock(&cache_mutex);
@@ -232,44 +266,19 @@ void *client_handler(void *arg) {
 }
 
 int main() {
+    logg("SERVER START", BACK_PURP);
     signal(SIGINT, sigint_handler);
-    int err;
-    struct sockaddr_in server_addr;
 
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
-        logg("Error while creating socket", RED);
+    int server_socket = create_server_socket();
+    if (server_socket == FAIL) {
+        logg("Error to create server socket", RED);
         exit(EXIT_FAILURE);
     }
+    logg_int("Server listening on port", PORT, BACK_WHITE);
 
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_UNSPEC;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
-    logg("Server socket created", GREEN);
-
-    err = bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    int err = init_cache();
     if (err == -1) {
-        logg("Failed to bind", RED);
-        close(server_socket);
-        exit(EXIT_FAILURE);
-    }
-    char buff[BUFFER_SIZE];
-    sprintf(buff, "Server socket bound to %d", server_addr.sin_addr.s_addr);
-    logg(buff, GREEN);
-
-    err = listen(server_socket, MAX_USERS_COUNT);
-    if (err == -1) {
-        logg("Failed to listen", RED);
-        close(server_socket);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Server listening on port %d\n", PORT);
-
-    err = init_cache();
-    if (err == -1) {
-        logg("Error while init cache", RED);
+        logg("Error to init cache", RED);
         destroy_cache();
         close(server_socket);
         exit(EXIT_FAILURE);
@@ -280,20 +289,28 @@ int main() {
         struct sockaddr_in client_addr;
         socklen_t client_addr_size = sizeof(client_addr);
         client_socket = accept(server_socket, (struct sockaddr *) &client_addr, &client_addr_size);
-        if (client_socket == -1) {
+        if (client_socket == FAIL) {
             logg("Failed to accept", RED);
             close(server_socket);
             destroy_cache();
             exit(EXIT_FAILURE);
         }
-        memset(buff, 0, strlen(buff));
+        char *buff = calloc(BUFFER_SIZE, sizeof(char));
         sprintf(buff, "Client connected from %s:%d", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
         logg(buff, BLUE);
+        free(buff);
 
-        char *request = read_request(client_socket);
-
-        if (find_in_cache(request, client_socket) == 1) {
+        char *request = calloc(BUFFER_SIZE, sizeof(char));
+        err = read_request(client_socket, request);
+        if (err == EXIT_FAILURE) {
             free(request);
+            close(client_socket);
+            continue;
+        }
+
+        if (find_in_cache(request, client_socket) == EXIT_SUCCESS) {
+            free(request);
+            close(client_socket);
             continue;
         } else {
             context ctx = {client_socket, request};
@@ -301,15 +318,18 @@ int main() {
             err = pthread_create(&handler_thread, NULL, &client_handler, &ctx);
             if (err == -1) {
                 logg("Failed to create thread", RED);
-                close(server_socket);
                 close(client_socket);
+                close(server_socket);
                 destroy_cache();
                 exit(EXIT_FAILURE);
             }
             pthread_join(handler_thread, NULL);
             free(request);
+            close(client_socket);
+
         }
     }
     close(server_socket);
     destroy_cache();
+    exit(EXIT_SUCCESS);
 }
